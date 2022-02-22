7db1e8683e585d36fd1b3e81e6a0cc28b430f94f
p0gchamp <52977370+p0gchamp@users.noreply.github.com>
2022-02-22 14:10:22 -0300

    initial commit

---
+++ examples/hello_world.rs
@@ -0,0 +1,149 @@
+use grr::{BlendChannel, BlendFactor, BlendOp, ClearAttachment, ColorBlend, ColorBlendAttachment, Framebuffer, Region, Viewport};
+use raw_gl_context::{GlConfig, GlContext, Profile};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use egui::{CtxRef};
+use epaint::{FontImage, TextureId};
+use egui_grr::{Painter, PainterSettings};
+
+
+fn main() -> anyhow::Result<()> {
+    unsafe {
+        let event_loop = EventLoop::new();
+
+        let window = WindowBuilder::new()
+            .with_title("egui")
+            .with_inner_size(LogicalSize::new(1024.0, 768.0))
+            .build(&event_loop)?;
+
+        let context = GlContext::create(
+            &window,
+            GlConfig {
+                version: (4, 5),
+                profile: Profile::Core,
+                red_bits: 8,
+                blue_bits: 8,
+                green_bits: 8,
+                alpha_bits: 0,
+                depth_bits: 0,
+                stencil_bits: 0,
+                samples: None,
+                srgb: true,
+                double_buffer: true,
+                vsync: true,
+            },
+        )
+            .unwrap();
+
+        context.make_current();
+
+        let mut ctx = CtxRef::default();
+
+        let mut winit = egui_winit::State::new(&window);
+
+        let grr = grr::Device::new(
+            |symbol| context.get_proc_address(symbol) as *const _,
+            grr::Debug::Enable {
+                callback: |report, _, _, _, msg| {
+                    println!("{:?}: {:?}", report, msg);
+                },
+                flags: grr::DebugReport::FULL,
+            },
+        );
+
+        let pixels : Vec<u8> = image::open("examples/grr.png").unwrap().to_rgba8().to_vec();
+        let mut painter = Painter::new(&grr, PainterSettings{
+            ibo_size: 10000,
+            vbo_size: 10000
+        });
+
+        let mut first_paint = true;
+
+
+        let mut text = String::with_capacity(128);
+
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Poll;
+
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                Event::WindowEvent {
+                    event,
+                    ..
+                } => {
+                    let request = winit.on_event(&ctx, &event);
+                    if request {
+                        window.request_redraw();
+                    }
+                }
+                Event::LoopDestroyed => {
+                    painter.free(&grr);
+                }
+                Event::RedrawRequested(_) => {
+                    {
+
+                        grr.set_viewport(0, &[Viewport{
+                            x: 0.0,
+                            y: 0.0,
+                            w: window.inner_size().width as _,
+                            h: window.inner_size().height as _,
+                            n: 0.0,
+                            f: 1.0
+                        }]);
+
+                        grr.set_scissor(0, &[Region {
+                            x: 0,
+                            y: 0,
+                            w: window.inner_size().width as _,
+                            h: window.inner_size().height as _,
+                        }]);
+                        grr.clear_attachment(Framebuffer::DEFAULT, ClearAttachment::ColorFloat(0, [0.3, 0.4, 0.1, 1.0]));
+                        grr.clear_attachment(Framebuffer::DEFAULT, ClearAttachment::DepthStencil(1.0, 0));
+
+                        let input = winit.take_egui_input(&window);
+
+                        let (output, shapes) = ctx.run(input, |ui|{
+                            egui::Area::new("si").show(&ui, |ui| {
+                                ui.heading("hello world!");
+                                if ui.button("quit").clicked() {
+                                    println!("quit");
+                                    *control_flow = ControlFlow::Exit;
+                                }
+                                ui.image(TextureId::User(0), (200.0, 200.0));
+                                ui.code_editor(&mut text);
+                            });
+                        });
+                        if first_paint{
+                            painter.set_user_texture(&grr, 0, &pixels, 454, 302);
+                            first_paint = !first_paint;
+                        }
+                        //dont do this
+                        painter.set_font_texture(&grr, TextureId::Egui, &ctx.font_image());
+
+                        winit.handle_output(&window, &ctx, output);
+
+
+                        let meshes = ctx.tessellate(shapes);
+                        // draw things behind egui here
+                        painter.paint(&grr, ctx.pixels_per_point(), window.inner_size().into(), meshes);
+
+                        // draw things on top of egui here
+                        context.swap_buffers();
+
+
+                    }
+                }
+                Event::MainEventsCleared => {
+                }
+                _ => (),
+            }
+        });
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,359 @@
+use epaint::{FontImage, TextureId};
+use grr::{BaseFormat, BlendChannel, BlendFactor, BlendOp, ClearAttachment, ColorBlend, ColorBlendAttachment, Constant, Extent, Filter, Format, FormatLayout, Framebuffer, HostImageCopy, ImageType, MemoryFlags, MemoryLayout, Offset, Region, SamplerAddress, SamplerDesc, ShaderFlags, ShaderSource, SubresourceLayers, VertexAttributeDesc, VertexBufferView, Viewport};
+use {
+    ahash::AHashMap,
+    egui::{emath::Rect, epaint::Mesh},
+};
+
+pub struct Painter {
+    max_texture_side: usize,
+    ibo : grr::Buffer,
+    vbo : grr::Buffer,
+    sampler : grr::Sampler,
+    pipeline: grr::Pipeline,
+    vertex_array: grr::VertexArray,
+
+    textures: AHashMap<egui::TextureId, (grr::ImageView, grr::Image)>,
+}
+#[derive(Copy, Clone, Debug)]
+pub struct PainterSettings{
+    pub ibo_size : u64,
+    pub vbo_size : u64
+}
+
+impl std::default::Default for PainterSettings {
+    fn default() -> Self {
+        Self{
+            ibo_size: 10048,
+            vbo_size: 10096
+        }
+    }
+}
+
+impl Painter {
+    pub fn new(device: &grr::Device, settings : PainterSettings) -> Painter {
+        let max_texture_side = 4096;
+
+        let vertex_array = unsafe {
+            let vao = device.create_vertex_array(&[VertexAttributeDesc {
+                location: 0,
+                binding: 0,
+                format: grr::VertexFormat::Xy32Float,
+                offset: 0,
+            }, VertexAttributeDesc {
+                location: 1,
+                binding: 0,
+                format: grr::VertexFormat::Xy32Float,
+                offset: (2 * std::mem::size_of::<f32>()) as _,
+            }, VertexAttributeDesc {
+                location: 2,
+                binding: 0,
+                format: grr::VertexFormat::Xyzw8Uint,
+                offset: (4 * std::mem::size_of::<f32>()) as _,
+            }
+            ]).expect("couldnt create a vertex array");
+            device.object_name(vao, "egui vao");
+            vao
+        };
+
+        let vbo = unsafe{
+            device.create_buffer(settings.vbo_size, MemoryFlags::DYNAMIC)
+        }.unwrap();
+
+        let ibo = unsafe {
+            device.create_buffer(settings.ibo_size, MemoryFlags::DYNAMIC)
+        }.unwrap();
+
+        let vertex_shader = unsafe {
+            let bytes = include_bytes!("shader/egui_vertex.glsl");
+            device.create_shader(grr::ShaderStage::Vertex, ShaderSource::Glsl, bytes, ShaderFlags::VERBOSE)
+        }
+            .expect("Failed to compile shader");
+
+        let fragment_shader = unsafe {
+            let bytes = include_bytes!("shader/egui_fragment.glsl");
+            device.create_shader(grr::ShaderStage::Fragment, ShaderSource::Glsl, bytes, ShaderFlags::VERBOSE)
+        }.expect("Failed to compile shader");
+
+        let pipeline = unsafe {
+            device.create_graphics_pipeline(
+                grr::GraphicsPipelineDesc {
+                    vertex_shader: Some(vertex_shader),
+                    tessellation_control_shader: None,
+                    tessellation_evaluation_shader: None,
+                    geometry_shader: None,
+                    fragment_shader: Some(fragment_shader),
+                    mesh_shader: None,
+                    task_shader: None,
+                },
+                grr::PipelineFlags::VERBOSE,
+            ).expect("failed to create a egui graphics pipeline")
+        };
+        let sampler = unsafe {
+            device.create_sampler(SamplerDesc {
+                min_filter: Filter::Linear,
+                mag_filter: Filter::Linear,
+                mip_map: None,
+                address: (SamplerAddress::ClampEdge, SamplerAddress::ClampEdge, SamplerAddress::ClampEdge),
+                lod_bias: 0.0,
+                lod: 0.0..10.0,
+                compare: None,
+                border_color: [0.0, 0.0, 0.0, 1.0],
+            })
+        }.expect("err sampler");
+
+        unsafe {
+            device.delete_shaders(&[vertex_shader, fragment_shader]);
+        }
+
+        Painter {
+            ibo,
+            vbo,
+            sampler,
+            max_texture_side,
+            pipeline,
+            vertex_array,
+            textures: Default::default(),
+        }
+    }
+
+    pub fn max_texture_side(&self) -> usize {
+        self.max_texture_side
+    }
+
+    pub fn paint(
+        &mut self,
+        device: &grr::Device,
+        pixels_per_point: f32,
+        dimensions: [u32; 2],
+        cipped_meshes: Vec<egui::ClippedMesh>,
+    ) {
+        for egui::ClippedMesh(clip_rect, mesh) in cipped_meshes {
+            self.paint_mesh(device, pixels_per_point, clip_rect, dimensions, &mesh);
+        }
+    }
+
+    #[inline(never)] // Easier profiling
+    fn paint_mesh(
+        &mut self,
+        device: &grr::Device,
+        pixels_per_point: f32,
+        clip_rect: Rect,
+        dimensions: [u32; 2],
+        mesh: &Mesh,
+    ) {
+        debug_assert!(mesh.is_valid());
+        if !mesh.is_valid(){
+            return;
+        }
+        {
+            #[repr(C)]
+            #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+            struct Vertex {
+                a_pos: [f32; 2],
+                a_tc: [f32; 2],
+                a_srgba: [u8; 4],
+            }
+
+            let vertices: &[Vertex] = bytemuck::cast_slice(&mesh.vertices);
+
+            unsafe { device.copy_host_to_buffer(self.vbo, 0, bytemuck::cast_slice(vertices))}
+        };
+        let vertex_buffer = self.vbo;
+        let indices = &mesh.indices.len();
+        let index_buffer = self.ibo;
+        unsafe { device.copy_host_to_buffer(self.ibo, 0, bytemuck::cast_slice(&mesh.indices)) }
+        let (width_in_pixels, height_in_pixels) = (dimensions[0], dimensions[1]);
+        let width_in_points = width_in_pixels as f32 / pixels_per_point;
+        let height_in_points = height_in_pixels as f32 / pixels_per_point;
+
+        if let Some((view, _image)) = self.get_texture(mesh.texture_id) {
+
+            //FIXME: annoying
+            // PERFORMANCE_WARNING: "Texture state usage warning: The texture object (0) bound to texture image unit 0 does not have a defined base level and cannot be used for texture mapping." ??
+            let screen_size = [width_in_points, height_in_points];
+
+            let clip_min_x = pixels_per_point * clip_rect.min.x;
+            let clip_min_y = pixels_per_point * clip_rect.min.y;
+            let clip_max_x = pixels_per_point * clip_rect.max.x;
+            let clip_max_y = pixels_per_point * clip_rect.max.y;
+
+            // Make sure clip rect can fit within a `u32`:
+            let clip_min_x = clip_min_x.clamp(0.0, width_in_pixels as f32);
+            let clip_min_y = clip_min_y.clamp(0.0, height_in_pixels as f32);
+            let clip_max_x = clip_max_x.clamp(clip_min_x, width_in_pixels as f32);
+            let clip_max_y = clip_max_y.clamp(clip_min_y, height_in_pixels as f32);
+
+            let clip_min_x = clip_min_x.round() as u32;
+            let clip_min_y = clip_min_y.round() as u32;
+            let clip_max_x = clip_max_x.round() as u32;
+            let clip_max_y = clip_max_y.round() as u32;
+
+
+            unsafe {
+                device.set_viewport(0, &[Viewport {
+                    x: 0.0,
+                    y: 0.0,
+                    w: width_in_pixels as f32,
+                    h: height_in_pixels as f32,
+                    n: 0.0,
+                    f: 1.0,
+                }]);
+                device.set_scissor(0, &[Region {
+                    x: clip_min_x as _,
+                    y: (height_in_pixels - clip_max_y) as _,
+                    w: (clip_max_x - clip_min_x) as _,
+                    h: (clip_max_y - clip_min_y) as _,
+                }]);
+
+
+                device.bind_pipeline(self.pipeline);
+                device.bind_color_blend_state(&ColorBlend{
+                    attachments: vec![ColorBlendAttachment{
+                        blend_enable: true,
+                        color: BlendChannel {
+                            src_factor: BlendFactor::One,
+                            dst_factor: BlendFactor::OneMinusSrcAlpha,
+                            blend_op: BlendOp::Add
+                        },
+                        alpha: BlendChannel {
+                            src_factor: BlendFactor::OneMinusDstAlpha,
+                            dst_factor: BlendFactor::One,
+                            blend_op: BlendOp::Add
+                        }
+                    }]
+                });
+
+                let sampler = self.sampler;
+
+                device.bind_vertex_array(self.vertex_array);
+                device.bind_vertex_buffers(self.vertex_array, 0, &[VertexBufferView {
+                    buffer: vertex_buffer,
+                    offset: 0,
+                    stride: ((std::mem::size_of::<f32>() * 4) + (std::mem::size_of::<u8>() * 4)) as _,
+                    input_rate: grr::InputRate::Vertex,
+                }]);
+                device.bind_uniform_constants(self.pipeline, 0, &[Constant::Vec2(screen_size)]);
+
+                device.bind_samplers(0, &[sampler]);
+                device.bind_image_views(0, &[*view]);
+
+                device.bind_index_buffer(self.vertex_array, index_buffer);
+                device.draw_indexed(grr::Primitive::Triangles, grr::IndexTy::U32, 0..(*indices as u32), 0..1, 0);
+
+                device.clear_buffer(vertex_buffer, Format::R8_UNORM, BaseFormat::R, FormatLayout::U8, None);
+                device.clear_buffer(index_buffer, Format::R8_UNORM, BaseFormat::R, FormatLayout::U8, None);
+            }
+        }
+    }
+
+    pub fn set_font_texture(
+        &mut self,
+        device: &grr::Device,
+        tex_id: egui::TextureId,
+        delta: &egui::epaint::FontImage,
+    ) {
+
+        let mut pixels = Vec::with_capacity(delta.height * delta.width * 4);
+        for c in delta.srgba_pixels(1.0){
+            pixels.push(c.r());
+            pixels.push(c.g());
+            pixels.push(c.b());
+            pixels.push(c.a());
+        }
+        let width = delta.width;
+        let height = delta.height;
+
+        if let Some((_view, image)) = self.textures.get(&tex_id) {
+            unsafe {
+                device.copy_host_to_image(&pixels, *image, HostImageCopy{
+                    host_layout: MemoryLayout {
+                        base_format: BaseFormat::RGBA,
+                        format_layout: FormatLayout::U8,
+                        row_length: width as u32,
+                        image_height: height as u32,
+                        alignment: 4
+                    },
+                    image_subresource: SubresourceLayers { level: 0, layers: 0..1 },
+                    image_offset: Offset {
+                        x: 0 as _,
+                        y: 0 as _,
+                        z: 0
+                    },
+                    image_extent: Extent {
+                        width: width as u32,
+                        height: height as u32,
+                        depth: 1
+                    }
+                });
+            };
+        } else {
+            let img = unsafe { self.copy_host_to_tex(&device, &pixels, width as u32, height as u32)};
+            self.textures.insert(tex_id, img);
+        }
+    }
+    pub fn set_user_texture(&mut self, device : &grr::Device, id : u64, pixels : &[u8], width : u32, height : u32){
+
+        let tex = unsafe { self.copy_host_to_tex(device, &pixels, width, height) } ;
+        self.textures.insert(TextureId::User(id), tex);
+    }
+
+    pub unsafe fn free_texture(&mut self, tex_id: egui::TextureId, device: &grr::Device) {
+        if let Some((view, img)) = self.textures.remove(&tex_id) {
+            device.delete_image_view(view);
+            device.delete_image(img);
+        }
+    }
+    unsafe fn copy_host_to_tex(&mut self, device : &grr::Device, pixels : &[u8], width : u32, height : u32) -> (grr::ImageView, grr::Image) {
+        let (tex, view) = device.create_image_and_view(ImageType::D2 {
+            width,
+            height,
+            layers: 1,
+            samples: 1,
+        }, grr::Format::R8G8B8A8_SRGB, 1).unwrap();
+
+        device.copy_host_to_image(pixels, tex, HostImageCopy {
+            host_layout: grr::MemoryLayout {
+                base_format: BaseFormat::RGBA,
+                format_layout: FormatLayout::U8,
+                row_length: width,
+                image_height: height,
+                alignment: 4,
+            },
+            image_subresource: grr::SubresourceLayers {
+                level: 0,
+                layers: 0..1,
+            },
+            image_offset: grr::Offset {
+                x: 0,
+                y: 0,
+                z: 0,
+            },
+            image_extent: grr::Extent {
+                width,
+                height,
+                depth: 1,
+            },
+        });
+        (view, tex)
+    }
+
+    fn get_texture(&self, texture_id: egui::TextureId) -> Option<&(grr::ImageView, grr::Image)> {
+        self.textures.get(&texture_id)
+    }
+    pub unsafe fn free(&mut self, device : &grr::Device) {
+        let mut keys = Vec::with_capacity(self.textures.len());
+        for key in self.textures.keys(){
+            keys.push(key.clone());
+        }
+        for i in keys{
+            self.free_texture(i, device);
+        }
+
+        device.delete_buffer(self.vbo);
+        device.delete_buffer(self.ibo);
+        device.delete_sampler(self.sampler);
+        device.delete_pipeline(self.pipeline);
+        device.delete_vertex_array(self.vertex_array);
+    }
+}
